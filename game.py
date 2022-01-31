e8de2f990017fd9a29e56cadb7b2b4097f6c083e
clarekwon2000 <clarekwon2000@gmail.com>
2022-01-31 15:27:25 -0500

    Update game.py

--- game.py
+++ game.py
@@ -5,3 +5,4 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+print("Rock, Paper, Scissors, Shoot!")
